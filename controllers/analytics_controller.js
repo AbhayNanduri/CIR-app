f2fdd7eadea4411f04d5b3890f75dda2949ff6bc
AbhayNanduri <ap279146@gmail.com>
2024-10-21 14:43:00 +0530

    Project Commit v.1.1

--- controllers/analytics_controller.js
+++ controllers/analytics_controller.js
@@ -3,7 +3,7 @@ const Student = require("../models/student");
 module.exports.showAnalytics = async (req, res) => {
   try {
     const students = await Student.find({});
-    res.sendFile('C:\Users\ap279\OneDrive\Desktop\AIE\Semester7\fds\Placement-cell\analytics.html', { root: './public' });
+    res.sendFile('analytics.html', { root: './public' });
   } catch (err) {
     console.error(err);
     res.status(500).send('Error fetching analytics data');
